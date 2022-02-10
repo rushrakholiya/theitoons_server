910e7cce20a757ba694c858152789657c6f9b807
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-10 15:47:55 +0530

    run migration

---
+++ app/Controllers/Migrate.php
@@ -0,0 +1,18 @@
+<?php
+ 
+namespace App\Controllers;
+
+class Migrate extends \CodeIgniter\Controller
+{
+    public function index()
+    {
+        $migrate = \Config\Services::migrations();
+
+        try {
+            $migrate->latest();
+            echo "Done";
+        } catch (\Throwable $e) {
+            echo "Not Done";// Do something with the error here...
+        }
+    }
+}
