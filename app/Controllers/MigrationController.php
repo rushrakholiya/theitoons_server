e6d5160701a8f8f77609513c47b5fdea5823a8ff
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-10 14:42:31 +0530

    run migration

--- app/Controllers/MigrationController.php
+++ app/Controllers/MigrationController.php
@@ -1,18 +1,20 @@
 <?php
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+defined("BASEPATH") or exit("No direct script access allowed");
 
 class MigrationController extends CI_Controller
 {
-        public function index($version){
-            $this->load->library("migration");
-
-            if(!$this->migration->version($version)){
-              show_error($this->migration->error_string());
-            }   
-            else
-            { 
-                 echo "<h2> Migration table has been created </h2>";
-            }
-        }
+         public function index()
+         {    
+               echo "<title> Tutorial and Example </title>"; 
+                 $this->load->library('migration'); // load migration library
+                 if ($this->migration->current() === False)
+                 {
+                         show_error($this->migration->error_string()); /* if current version is not found, it returns an error message. */
+                 }
+                 else
+                 { 
+                     echo "<h2> Migration table has been created </h2>";
+                 }
+         }
 }
