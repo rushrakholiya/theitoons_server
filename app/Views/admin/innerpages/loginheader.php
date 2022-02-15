491a524d8d2d18c31cffaca0f8476b0f18a7eaf0
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-15 20:50:28 +0530

    admin

--- app/Views/admin/innerpages/loginheader.php
+++ app/Views/admin/innerpages/loginheader.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <?php echo base_url("/public/assets");?>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo esc($title); ?></title>
