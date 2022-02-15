75ef383d600e26b1bc86d4ac4c424500712426e4
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-15 18:05:20 +0530

    admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -6,46 +6,38 @@ class Admin extends BaseController
 {
     public function index()
     {        
-        $baseurl = base_url("/public/assets");
-        $siteurl = base_url();
         $data = [];
         $data['title']         = 'Login - TheIToons';
         $data['main_content']  = 'admin/login';// page name
-        $data['base_url']      = $baseurl;
-        $data['site_url']      = $siteurl;
+        $data['base_url']      = baseurl();
+        $data['site_url']      = siteurl();
         echo view('admin/innerpages/logintemplate',$data);
     }
     public function register()
     {
-        $baseurl = base_url("/public/assets");
-        $siteurl = base_url();
         $data = [];
         $data['title']         = 'Registration - TheIToons';
         $data['main_content']  = 'admin/register';// page name
-        $data['base_url']      = $baseurl;
-        $data['site_url']      = $siteurl;
+        $data['base_url']      = baseurl();
+        $data['site_url']      = siteurl();
         echo view('admin/innerpages/logintemplate',$data);
     }
     public function forgot_password()
     {
-        $baseurl = base_url("/public/assets");
-        $siteurl = base_url();
         $data = [];
         $data['title']         = 'Forgot Password - TheIToons';
         $data['main_content']  = 'admin/forgot_password';// page name
-        $data['base_url']      = $baseurl;
-        $data['site_url']      = $siteurl;
+        $data['base_url']      = baseurl();
+        $data['site_url']      = siteurl();
         echo view('admin/innerpages/logintemplate',$data);
     }
     public function dashboard()
     {
-        $baseurl = base_url("/public/assets");
-        $siteurl = base_url();
         $data = [];
         $data['title']         = 'Dashboard - TheIToons';
         $data['main_content']  = 'admin/dashboard';// page name
-        $data['base_url']      = $baseurl;
-        $data['site_url']      = $siteurl;
+        $data['base_url']      = baseurl();
+        $data['site_url']      = siteurl();
         echo view('admin/innerpages/template',$data);
     }
     
