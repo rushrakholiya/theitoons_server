c73997d51e85d403d5ccce0d88f438756d8ef92f
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-15 20:38:18 +0530

    admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,35 +2,23 @@
  
 namespace App\Controllers;
 
-class Admin extends BaseController
+class Admin extends HF_Controller
 {
     public function index()
     {        
-        $data = [];
-        $data['title']         = 'Login - TheIToons';
-        $data['main_content']  = 'admin/login';// page name
-        echo view('admin/innerpages/logintemplate',$data);
+        $this->loginheaderfooter('login',$data);
     }
     public function register()
     {
-        $data = [];
-        $data['title']         = 'Registration - TheIToons';
-        $data['main_content']  = 'admin/register';// page name
-        echo view('admin/innerpages/logintemplate',$data);
+        $this->loginheaderfooter('register',$data);
     }
     public function forgot_password()
     {
-        $data = [];
-        $data['title']         = 'Forgot Password - TheIToons';
-        $data['main_content']  = 'admin/forgot_password';// page name
-        echo view('admin/innerpages/logintemplate',$data);
+        $this->loginheaderfooter('forgot_password',$data);
     }
     public function dashboard()
     {
-        $data = [];
-        $data['title']         = 'Dashboard - TheIToons';
-        $data['main_content']  = 'admin/dashboard';// page name
-        echo view('admin/innerpages/template',$data);
+        $this->headerfooter('dashboard',$data);
     }
     
 }
