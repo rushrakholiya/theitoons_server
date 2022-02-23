3e2ce38f23b50da5c9fab872ea88b306b3ba4581
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-23 12:51:37 +0530

    registration

--- app/Views/admin/register.php
+++ app/Views/admin/register.php
@@ -29,8 +29,7 @@
               <span class="fas fa-user"></span>
             </div>
           </div>
-          <?php if(isset($validation)){?>
-          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'username'); ?></span><?php }?>         
+          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'username'); ?></span>        
         </div>
 
         <div class="input-group mb-3">
@@ -40,8 +39,7 @@
               <span class="fas fa-envelope"></span>
             </div>
           </div>
-          <?php if(isset($validation)){?>
-          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'email'); ?></span><?php }?> 
+          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'email'); ?></span>
         </div>
         <div class="input-group mb-3">
           <input type="password" class="form-control" placeholder="Password *" name="password">
@@ -50,8 +48,7 @@
               <span class="fas fa-lock"></span>
             </div>
           </div>
-          <?php if(isset($validation)){?>
-          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'password'); ?></span><?php }?> 
+          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'password'); ?></span>
         </div>
         <div class="input-group mb-3">
           <input type="password" class="form-control" placeholder="Retype password *" name="pass_confirm">
@@ -60,8 +57,7 @@
               <span class="fas fa-lock"></span>
             </div>
           </div>
-          <?php if(isset($validation)){?>
-          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'pass_confirm'); ?></span><?php }?> 
+          <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'pass_confirm'); ?></span>
         </div>
         <div class="row mb-3">
           <div class="col-12">
@@ -71,8 +67,7 @@
                Please confirm that you agree to our <a href="#">privacy policy</a>
               </label>
             </div>
-            <?php if(isset($validation)){?>
-            <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'terms'); ?></span><?php }?> 
+            <span class="text-danger input-group mt-1 ml-1"><?= display_error($validation,'terms'); ?></span>
           </div>
         </div>
         <div class="row justify-content-center">
