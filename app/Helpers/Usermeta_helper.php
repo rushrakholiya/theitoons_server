3cf4245d7a4d9bc89de60f9310e8090c6b6f504b
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-23 12:58:13 +0530

    registration

--- app/Helpers/Usermeta_helper.php
+++ app/Helpers/Usermeta_helper.php
@@ -1,22 +1,4 @@
 <?php
-if(!function_exists('display_error')) {
-	function display_error($validation,$field)
-	{
-		if(isset($validation))
-		{
-			if($validation->hasError($field))
-			{
-				return $validation->getError($field);
-			}
-			else
-			{
-				return false;
-			}
-
-		}
-
-	}
-}
 if(!function_exists('getUserMeta')) {
 	function getUserMeta($metakey,$id)
 	{
