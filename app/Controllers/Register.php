bbf1478571475a011afd9aa693f7bc8bd4c8fc0a
SurbhiJethva <surbhijethva216@gmail.com>
2022-02-23 12:58:22 +0530

    registration

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -9,7 +9,7 @@ class Register extends HF_Controller
     public $registerModel;
     public function __construct()
     {
-        helper(['form', 'url', 'usermeta']);
+        helper(['form', 'url']);
         $this->registerModel = new RegisterModel();
         $this->session = \Config\Services::session();
     }
