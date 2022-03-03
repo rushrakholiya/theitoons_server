89df9f607c41ccde595432fb0b04ce0e4ed59125
SurbhiJethva <surbhijethva216@gmail.com>
2022-03-03 19:23:56 +0530

    taskrequest

---
+++ app/Database/Seeds/SiteDetailSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class SiteDetailSeeder extends Seeder
+{
+    public function run()
+    {
+        $generaldata = [
+            ['option_name' => 'site_name','option_value' => "",],
+            ['option_name' => 'site_favicon','option_value' => "",],
+            ['option_name' => 'site_logo','option_value' => "",],  
+            ['option_name' => 'admin_email','option_value' => "",],
+            ['option_name' => 'paypal_enable_disable','option_value' =>"" ,],
+            ['option_name' => 'paypal_sandbox','option_value' =>"" ,],
+            ['option_name' => 'paypal_email','option_value' => "",],   
+            ['option_name' => 'receiver_email','option_value' => "",],
+            ['option_name' => 'live_API_username','option_value' => "",],
+            ['option_name' => 'live_API_password','option_value' => "",],
+            ['option_name' => 'live_API_signature','option_value' => "",],
+        ];
+        $builder = $this->db->table('site_details');
+        $res = $builder->insertBatch($generaldata);
+    }
+}
