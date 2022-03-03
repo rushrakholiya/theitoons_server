c8e7f53db3d6a3dfb5e79720ecfcbebbb35b19ae
SurbhiJethva <surbhijethva216@gmail.com>
2022-03-03 19:49:15 +0530

    taskrequest

--- app/Controllers/HF_Controller.php
+++ app/Controllers/HF_Controller.php
@@ -4,9 +4,10 @@ namespace App\Controllers;
 class HF_Controller extends BaseController
 {    
     function loginheaderfooter($template_name,$data){
-        if (! is_file(APPPATH . 'Views/' . $template_name . '.php')) {
+        print_r($template_name);
+        /*if (! is_file(APPPATH . 'Views/' . $template_name . '.php')) {
             throw new \CodeIgniter\Exceptions\PageNotFoundException($template_name);
-        }
+        }*/
         $site_name = getGeneralData("site_name");
         if(!empty($site_name->option_value))
         {$sitename=$site_name->option_value;}else{$sitename="TheIToons";}
