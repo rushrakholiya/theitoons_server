14c41d966a80d963614bf5754abf203b8826abaa
SurbhiJethva <surbhijethva216@gmail.com>
2022-04-09 19:27:04 +0530

    paypaladminintegrationwithfront

--- app/Controllers/Payment.php
+++ app/Controllers/Payment.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Libraries\Omnipaygateway; 
+use App\Models\PaymentModel;
 
 class Payment extends HF_Controller
 {    
@@ -10,7 +11,7 @@ class Payment extends HF_Controller
     {
         helper(['form', 'url','usererror']);
         $this->session = \Config\Services::session();
-        $this->purchaseProc = new Omnipaygateway('PayPal_Express', true);
+        $this->paymentModel = new PaymentModel();
     }
     public function index()
     {    
@@ -21,48 +22,74 @@ class Payment extends HF_Controller
         }
         else
         {
-            echo "Paypal Integration";
-            if(isset($_GET['token']) && isset($_GET['PayerID'])){
-                $parameters = array(
-                    'amount' => 10.00,
-                    'currency'=>'USD',
-                    'token' => $_GET['token'],
-                    'payerid' => $_GET['PayerID'],
-                    'returnUrl'=> base_url()."/payment",
-                    'cancelUrl'=> base_url()."/payment");
-                $datacomplete = $this->purchaseProc->complete($parameters);
-                echo '<pre>'; 
-                print_r($datacomplete);
-                echo '</pre>';
-            }
-            elseif(isset($_GET['token']))
+            $paymentdata = $this->paymentModel->verifyPaymentMethod();            
+            if(!empty($paymentdata))
             {
-                echo "<br><br>You have cancelled your recent PayPal payment, Please try again!<br>";
-                $valTransc = array(
-                    'amount' => 10.00,
-                    'currency'=>'USD',
-                    'returnUrl'=> base_url()."/payment",
-                    'cancelUrl'=> base_url()."/payment");
-                $datapurchase = $this->purchaseProc->sendPurchase($valTransc);
-                echo '<pre>'; 
-                print_r($datapurchase);
-                echo '</pre>';
-            } 
+                //print_r($paymentdata);
+                foreach($paymentdata as $row){
+                    if($row['option_name'] == 'paypal_sandbox' )
+                        {$paypal_sandbox = $row['option_value'];}
+                    if($row['option_name'] == 'live_API_username' )
+                        {$live_API_username = $row['option_value'];;}
+                    if($row['option_name'] == 'live_API_password' )
+                        {$live_API_password = $row['option_value'];}
+                    if($row['option_name'] == 'live_API_signature' )
+                        {$live_API_signature = $row['option_value'];}                   
+                }
+                if($paypal_sandbox==1){$test_mode = true;}else{$test_mode = false;}
+                
+                $this->purchaseProc = new Omnipaygateway('PayPal_Express', $test_mode,$live_API_username,$live_API_password,$live_API_signature);
+
+                /*echo "Paypal Integration";*/
+                if(isset($_GET['token']) && isset($_GET['PayerID'])){
+                    $parameters = array(
+                        'amount' => 10.00,
+                        'currency'=>'USD',
+                        'token' => $_GET['token'],
+                        'payerid' => $_GET['PayerID'],
+                        'returnUrl'=> base_url()."/payment",
+                        'cancelUrl'=> base_url()."/payment");
+                    $datacomplete = $this->purchaseProc->complete($parameters);
+                    /*echo '<pre>'; 
+                    print_r($datacomplete);
+                    echo '</pre>';*/
+                    $data['datacomplete'] = $datacomplete;
+                    return $this->loginheaderfooter('payment',$data);
+                }
+                elseif(isset($_GET['token']))
+                {
+                    $valTransc = array(
+                        'amount' => 10.00,
+                        'currency'=>'USD',
+                        'returnUrl'=> base_url()."/payment",
+                        'cancelUrl'=> base_url()."/payment");
+                    $datapurchasec = $this->purchaseProc->sendPurchase($valTransc);
+                    /*echo '<pre>'; 
+                    print_r($datapurchasec);
+                    echo '</pre>';*/
+                    $data['datapurchasec'] = $datapurchasec;
+                    return $this->loginheaderfooter('payment',$data);
+                } 
+                else
+                {
+                    $valTransc = array(
+                        'amount' => 10.00,
+                        'currency'=>'USD',
+                        'returnUrl'=> base_url()."/payment",
+                        'cancelUrl'=> base_url()."/payment");
+                    $datapurchase = $this->purchaseProc->sendPurchase($valTransc);
+                    /*echo '<pre>'; 
+                    print_r($datapurchase);
+                    echo '</pre>';*/
+                    $data['datapurchase'] = $datapurchase;
+                    return $this->loginheaderfooter('payment',$data);
+                }            
+            }
             else
             {
-                $valTransc = array(
-                    'amount' => 10.00,
-                    'currency'=>'USD',
-                    'returnUrl'=> base_url()."/payment",
-                    'cancelUrl'=> base_url()."/payment");
-                $datapurchase = $this->purchaseProc->sendPurchase($valTransc);
-                echo '<pre>'; 
-                print_r($datapurchase);
-                echo '</pre>';
-
+                $data['paypalerror'] = "Paypal Disabled, Try again.";
+                return $this->loginheaderfooter('payment',$data);
             }
-            
-            
         }    
     }
 }
