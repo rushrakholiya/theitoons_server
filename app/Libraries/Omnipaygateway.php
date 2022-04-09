87b97872e178e895238b6f29fbdc9e210dcbe6f7
SurbhiJethva <surbhijethva216@gmail.com>
2022-04-09 19:30:35 +0530

    paypaladminintegrationwithfront

--- app/Libraries/Omnipaygateway.php
+++ app/Libraries/Omnipaygateway.php
@@ -11,10 +11,6 @@ class Omnipaygateway extends Omnipay {
     public function __construct($set_gateway,$test_mode,$live_API_username,$live_API_password,$live_API_signature)
     {
         $this->gateway = Omnipay::create($set_gateway); 
-        /*$this->gateway->setUsername('rushabhpaypal-facilitator_api1.gmail.com');
-        $this->gateway->setPassword('FUTEDYRAG3AEBA5A');
-        $this->gateway->setSignature('AFcWxV21C7fd0v3bYYYRCpSSRl31A58fGU4N.nfYx2zgzMXid7PRQPWQ');
-        $this->gateway->setTestMode($test_mode);*/
         $this->gateway->setUsername($live_API_username);
         $this->gateway->setPassword($live_API_password);
         $this->gateway->setSignature($live_API_signature);
