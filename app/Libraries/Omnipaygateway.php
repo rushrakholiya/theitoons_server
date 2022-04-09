6e27232befac3a99181bec4ee7cbae6844322b76
SurbhiJethva <surbhijethva216@gmail.com>
2022-04-09 19:27:17 +0530

    paypaladminintegrationwithfront

--- app/Libraries/Omnipaygateway.php
+++ app/Libraries/Omnipaygateway.php
@@ -7,12 +7,17 @@ class Omnipaygateway extends Omnipay {
 
     protected $gateway = null;
 
-    public function __construct($set_gateway='PayPal_Express',$test_mode=true)
+    //public function __construct($set_gateway='PayPal_Express',$test_mode=true)
+    public function __construct($set_gateway,$test_mode,$live_API_username,$live_API_password,$live_API_signature)
     {
-        $this->gateway = Omnipay::create($set_gateway);
-        $this->gateway->setUsername('rushabhpaypal-facilitator_api1.gmail.com');
+        $this->gateway = Omnipay::create($set_gateway); 
+        /*$this->gateway->setUsername('rushabhpaypal-facilitator_api1.gmail.com');
         $this->gateway->setPassword('FUTEDYRAG3AEBA5A');
         $this->gateway->setSignature('AFcWxV21C7fd0v3bYYYRCpSSRl31A58fGU4N.nfYx2zgzMXid7PRQPWQ');
+        $this->gateway->setTestMode($test_mode);*/
+        $this->gateway->setUsername($live_API_username);
+        $this->gateway->setPassword($live_API_password);
+        $this->gateway->setSignature($live_API_signature);
         $this->gateway->setTestMode($test_mode);
     }
 
@@ -32,9 +37,9 @@ class Omnipaygateway extends Omnipay {
         }
         elseif($response->isRedirect())
         {
-            if ($response->getRedirectMethod() == "GET"){?>
-            <h4>Amount : <?= $valTransaction['amount']." ".$valTransaction['currency'];?></h4>
-            <p><a href="<?= $response->getRedirectUrl();?>" class="btn btn-success">Pay Now </a></p><?php 
+            if ($response->getRedirectMethod() == "GET"){
+                /*$paypalResponse = "<h4>Amount : ".$valTransaction['amount']." ".$valTransaction['currency']."</h4><p><a href=".$response->getRedirectUrl()." class='btn btn-success'>Pay Now </a></p>";*/
+                $paypalResponse = "<p><a href=".$response->getRedirectUrl()." class='btn btn-primary btn-block'>Pay with paypal</a></p>";
             } 
         }
         else
