dcae974e78ca61c236e2100927ff3cfd8c4e5713
SurbhiJethva <surbhijethva216@gmail.com>
2022-04-30 19:31:13 +0530

    taskrequest3

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -22,7 +22,6 @@ class Dashboard extends HF_Controller
         else
         {
             $data = [];
-            $data['validation'] = null;
             $data['totaltaskno'] = null;
             if(session()->has('logged_user_client')){$uid=session()->get('logged_user_client'); }
 
@@ -145,7 +144,7 @@ class Dashboard extends HF_Controller
             }
         }
     }
-    /*public function editTaskRequest($id=null)
+    public function editTaskRequest($id=null)
     {
         if(!session()->has('logged_user_client'))
         {
@@ -153,18 +152,73 @@ class Dashboard extends HF_Controller
         }
         else
         {
-            $data = [];
-            $completeresponse = $this->dashboardModel->completeTaskRequest($id);
-            if( $completeresponse == true )
-            {            
-                $this->session->setTempdata('success','Task Request Completed Successfully.',2);
-                return redirect()->to(base_url().'/dashboard');
+            $data = [];$uid = "";
+            if(session()->has('logged_user_client')){
+                $uid=session()->get('logged_user_client'); 
+            }
+            $checkauthorizeduser = $this->dashboardModel->checkAuthorizedUser($id,$uid);
+            if( $checkauthorizeduser == true )
+            {
+
+                if( $this->request->getMethod() == "post" ){                    
+                    $path = "";
+                    $file =  $this->request->getFile('reference');
+                    if(empty($file->getName())){
+                        $reference_img = getTaskRequestMeta("reference_img", $id);
+                        if(!empty($reference_img->meta_value)){
+                            $path = $reference_img->meta_value;
+                        }
+                    }
+                    else{
+                        if($file->move(FCPATH.'public/taskrequest', $file->getName()))
+                        {
+                            $path = base_url().'/public/taskrequest/'.$file->getName();
+                        } 
+                    }
+
+                    $taskdata = [
+                        'task_title' => $this->request->getVar('title'),
+                    ];
+                    $taskmetadata = [
+                    ['meta_key' => 'requesttype','meta_value' => $this->request->getVar('requesttype'),],
+                    ['meta_key' => 'priority','meta_value' => $this->request->getVar('priority'),],
+                    ['meta_key' => 'task_description','meta_value' => $this->request->getVar('task_description'),],
+                    ['meta_key' => 'reference_img','meta_value' => $path,],
+                    ['meta_key' => 'constraint','meta_value' => $this->request->getVar('constraint'),],
+                    ['meta_key' => 'deadline','meta_value' => $this->request->getVar('deadline'),],
+                    ['meta_key' => 'budget','meta_value' => $this->request->getVar('budget'),],
+                    ];
+                    if($this->dashboardModel->editTaskRequest($id,$taskdata,$taskmetadata))
+                    {
+                        $this->session->setTempdata('success','Task Request updated Successfully.',2);
+                        return redirect()->to(base_url().'/dashboard');
+                    }
+                    else
+                    {
+                        $this->session->setTempdata('error','Sorry! Task Request not updated, Try again.',2);
+                        return redirect()->to(base_url().'/dashboard');
+                    }            
+                }
+                else
+                {
+                    $data['taskrequestinfo'] = $this->dashboardModel->viewTaskRequest($id);
+                    if(!empty($data['taskrequestinfo']))
+                    {
+                        return $this->loginheaderfooter('editTaskRequest',$data);
+                    }
+                    else
+                    { 
+                        $data['error'] = "Sorry! No Records found, Try again.";
+                        return $this->loginheaderfooter('editTaskRequest',$data);
+                    }
+                    //return $this->loginheaderfooter('dashboard/editTaskRequest/'.$id,$data);
+                }
             }
             else
-            { 
-                $this->session->setTempdata('error','Not Completed. Try again.',2);
+            {
+                $this->session->setTempdata('error','Sorry, You are not authorised user to view the request.',2);
                 return redirect()->to(base_url().'/dashboard');
             }
         }
-    }*/
+    }
 }
