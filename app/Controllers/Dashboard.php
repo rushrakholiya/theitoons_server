a34f3bb909c2818e556c9293a35b84ac351d9632
SurbhiJethva <surbhijethva216@gmail.com>
2022-04-30 13:44:30 +0530

    taskrequest2

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -48,4 +48,123 @@ class Dashboard extends HF_Controller
                                
         }    
     }
+    public function deleteTaskRequest($id=null)
+    {
+        if(!session()->has('logged_user_client'))
+        {
+            return redirect()->to(base_url()."/login");
+        }
+        else
+        {
+            $data = [];
+            if(session()->has('logged_user_client')){$uid=session()->get('logged_user_client'); }
+            $checkauthorizeduser = $this->dashboardModel->checkAuthorizedUser($id,$uid);
+            if( $checkauthorizeduser == true )
+            {
+                $deleteresponse = $this->dashboardModel->deleteTaskRequest($id);
+                if( $deleteresponse == true )
+                {            
+                    $this->session->setTempdata('success','Task Request Deleted Successfully.',2);
+                    return redirect()->to(base_url().'/dashboard');
+                }
+                else
+                { 
+                    $this->session->setTempdata('error','Not Deleted. Try again.',2);
+                    return redirect()->to(base_url().'/dashboard');
+                }
+            }
+            else
+            {
+                $this->session->setTempdata('error','Sorry, You are not authorised user to delete the request.',2);
+                return redirect()->to(base_url().'/dashboard');
+            }
+
+        }
+    }
+    public function completeTaskRequest($id=null)
+    {
+        if(!session()->has('logged_user_client'))
+        {
+            return redirect()->to(base_url()."/login");
+        }
+        else
+        {
+            $data = [];
+            if(session()->has('logged_user_client')){$uid=session()->get('logged_user_client'); }
+            $checkauthorizeduser = $this->dashboardModel->checkAuthorizedUser($id,$uid);
+            if( $checkauthorizeduser == true )
+            {
+                $completeresponse = $this->dashboardModel->completeTaskRequest($id);
+                if( $completeresponse == true )
+                {            
+                    $this->session->setTempdata('success','Task Request Completed Successfully.',2);
+                    return redirect()->to(base_url().'/dashboard');
+                }
+                else
+                { 
+                    $this->session->setTempdata('error','Not Completed. Try again.',2);
+                    return redirect()->to(base_url().'/dashboard');
+                }
+            }
+            else
+            {
+                $this->session->setTempdata('error','Sorry, You are not authorised user to complete the request.',2);
+                return redirect()->to(base_url().'/dashboard');
+            }
+        }
+    }
+    public function viewTaskRequest($id=null)
+    {
+        if(!session()->has('logged_user_client'))
+        {
+            return redirect()->to(base_url()."/login");
+        }
+        else
+        {
+            $data = [];
+            if(session()->has('logged_user_client')){$uid=session()->get('logged_user_client'); }
+            $checkauthorizeduser = $this->dashboardModel->checkAuthorizedUser($id,$uid);
+            if( $checkauthorizeduser == true )
+            {
+                $data['taskrequestinfo'] = $this->dashboardModel->viewTaskRequest($id);
+                if(!empty($data['taskrequestinfo']))
+                {
+                    return $this->loginheaderfooter('viewTaskRequest',$data);
+                }
+                else
+                { 
+                    $data = [];
+                    $data['error'] = "Sorry! No Records found, Try again.";
+                    return $this->loginheaderfooter('viewTaskRequest',$data);
+                }
+            }
+            else
+            {
+                $this->session->setTempdata('error','Sorry, You are not authorised user to view the request.',2);
+                return redirect()->to(base_url().'/dashboard');
+            }
+        }
+    }
+    /*public function editTaskRequest($id=null)
+    {
+        if(!session()->has('logged_user_client'))
+        {
+            return redirect()->to(base_url()."/login");
+        }
+        else
+        {
+            $data = [];
+            $completeresponse = $this->dashboardModel->completeTaskRequest($id);
+            if( $completeresponse == true )
+            {            
+                $this->session->setTempdata('success','Task Request Completed Successfully.',2);
+                return redirect()->to(base_url().'/dashboard');
+            }
+            else
+            { 
+                $this->session->setTempdata('error','Not Completed. Try again.',2);
+                return redirect()->to(base_url().'/dashboard');
+            }
+        }
+    }*/
 }
