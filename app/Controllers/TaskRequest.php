23d7fcaee62b05d54d225fe87d1d001ff31edc04
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-04 22:17:13 +0530

    taskrequest4

--- app/Controllers/TaskRequest.php
+++ app/Controllers/TaskRequest.php
@@ -86,14 +86,14 @@ class TaskRequest extends HF_Controller
 
                         /*$admin_email = getGeneralData("admin_email");
                         if(!empty($admin_email->option_value)){$admin_email=$admin_email->option_value;}else{$admin_email="me@preraktrivedi.com";}*/
-                        $admin_email = "prerak@theitoons.com";
+                        $admin_email = "surbhijethva216@gmail.com";
                         //sent mail to user
                         $to = "surbhi@theitoons.com";//$useremail;
                         $subject = 'Thank you! | '.$sitename;
                         $message = "";
-                        $message .= 'Dear user ('.$username.'),<br><br>Thank you for contacting us!';
+                        $message .= 'Dear user ('.$username.'),<br><br>Thank you for contacting us!<br><br>';
                         $message .= '<table cellpadding="5"><tbody><tr><th valign="top" align="right">Email:</th><td>'.$useremail.'</td></tr><tr><th valign="top" align="right">Type:</th><td>'.$requesttype->meta_value.'</td></tr><tr><th valign="top" align="right">Task title:</th><td>'.$this->request->getVar('title').'</td></tr><tr><th valign="top" align="right">Priority:</th><td>'.$priority->meta_value.'</td></tr><tr><th valign="top" align="right">Task description:</th><td>'.$task_description->meta_value.'</td></tr><tr><th valign="top" align="right">Reference files:</th><td>'.$refimgname[0].'</td></tr><tr><th valign="top" align="right">Constraint:</th><td>'.$constraint->meta_value.'</td></tr><tr><th valign="top" align="right">Deadline:</th><td>'.$deadline->meta_value.'</td></tr><tr><th valign="top" align="right">Estimated budget:</th><td>$'.$budget->meta_value.'</td></tr></tbody></table>';
-                        $message .= 'We will reply within 48 hours.<br>Best Regards, '.$sitename.'<br><br>';
+                        $message .= '<br><br>We will reply within 48 hours.<br><br>Best Regards, '.$sitename.'<br><br>';
                         if(!empty($refimg)){ $message .= '<img src='.$refimg.'/>';}
                         $email = \Config\Services::email();
                         $email->setHeader('Content-Type', 'text/html; charset=UTF-8\r\n');
