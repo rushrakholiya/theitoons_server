7000a5ebd610429332d456d7e211cd6b0de92915
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-05 12:07:01 +0530

    setcredencials

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,6 +6,17 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
+    
+    public function __construct()
+    {
+        helper(['usererror']);
+    }
+    $smtpadmin_email = getGeneralData("smtpadmin_email");
+    if(!empty($smtpadmin_email->option_value)){$smtpadmin_email=$admin_email->option_value;}
+
+    $smtpadmin_pass = getGeneralData("smtpadmin_pass");
+    if(!empty($smtpadmin_pass->option_value)){$smtpadmin_pass=$admin_email->option_value;}
+
     /**
      * @var string
      */
@@ -54,14 +65,14 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $SMTPUser;
+    public $SMTPUser = $admin_email;
 
     /**
      * SMTP Password
      *
      * @var string
      */
-    public $SMTPPass;
+    public $SMTPPass = $smtpadmin_pass;
 
     /**
      * SMTP Port
