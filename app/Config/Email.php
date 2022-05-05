c539cda1a9a8732f2d92f59c0800dff6c180b1e4
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-05 13:04:11 +0530

    setcredencials

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -54,14 +54,14 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $SMTPUser = 'prerak@theitoons.com';
+    public $SMTPUser;
 
     /**
      * SMTP Password
      *
      * @var string
      */
-    public $SMTPPass = 'Prerak-Private-TI';
+    public $SMTPPass;
 
     /**
      * SMTP Port
