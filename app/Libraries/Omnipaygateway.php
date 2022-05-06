623a6f76ebe4ed69be3f42a6d1e5ac09270f3f46
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-06 22:29:32 +0530

    paypalrestintegration

--- app/Libraries/Omnipaygateway.php
+++ app/Libraries/Omnipaygateway.php
@@ -10,19 +10,12 @@ class Omnipaygateway extends Omnipay {
     //public function __construct($set_gateway='PayPal_Express',$test_mode=true)
     public function __construct($set_gateway,$test_mode,$live_API_username,$live_API_password,$live_API_signature)
     {
-        /*$this->gateway = Omnipay::create($set_gateway); 
-        $this->gateway->setUsername($live_API_username);
-        $this->gateway->setPassword($live_API_password);
-        $this->gateway->setSignature($live_API_signature);
-        $this->gateway->setTestMode($test_mode);*/
-
         $this->gateway = Omnipay::create($set_gateway);
-
         // Initialise the gateway
         $this->gateway->initialize(array(
-            'clientId' => 'AeHkW6pzEEODPHaZH4W1Gk7B73rbJgmVVU7gzKKUaYFpGi5qF9GMYrWSd1awyvjBmIA6thWUWfXODREx',
-            'secret'   => 'ECVGJOn88VTJ9vBq8Lvhju6fu6WBQnwNNEXNN1-QoAXkV9TvtnDXpLdJGQf5EKReqFtIG06tsxQapqAh',
-           'testMode' => $test_mode, // Or false when you are ready for live transactions
+            'clientId' => $live_API_password,
+            'secret'   => $live_API_signature,
+            'testMode' => $test_mode, // Or false when you are ready for live transactions
         ));
     }
 
@@ -37,15 +30,6 @@ class Omnipaygateway extends Omnipay {
             'cancelUrl' => $valTransaction['cancelUrl']
         );
         $response = $this->gateway->purchase($payArray)->send();
-        /*echo "<pre>";
-        print_r($response);
-        echo "</pre>";
-        $data = $response->getData();
-        echo "Gateway purchase response data == ";
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
-        echo $response->getRedirectUrl();exit;*/
         if($response->isRedirect())
         {
             $response->redirect();            
