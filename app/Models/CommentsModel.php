19d04023e8cc22e65a54116eac31d438c0bb9942
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-30 15:25:23 +0530

    updatecommentsection

--- app/Models/CommentsModel.php
+++ app/Models/CommentsModel.php
@@ -13,12 +13,13 @@ class CommentsModel extends Model
 			$builder->select('user_meta.user_id');
 			$query = $builder->getWhere(['meta_key'=> 'user_manager','meta_value' => $auid],1);
 		    $userid = $query->getRow();
-
-			$builder = $this->db->table('task_request_comments');
-			$builder->select('*');
-			$query = $builder->where('user_id',$userid->user_id);
-			$result = $builder->get();
-			return $result->getResultArray();
+		    if(!empty($userid)){
+					$builder = $this->db->table('task_request_comments');
+					$builder->select('*');
+					$query = $builder->where('user_id',$userid->user_id);
+					$result = $builder->get();
+					return $result->getResultArray();
+			}
 		}else{
 			$builder = $this->db->table('task_request_comments');
 			$builder->select('*');
