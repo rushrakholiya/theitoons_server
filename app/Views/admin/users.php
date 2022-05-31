b5d197ee0291753c476f4dbcf42df88ebbaab46d
SurbhiJethva <surbhijethva216@gmail.com>
2022-05-31 12:58:28 +0530

    displaymanagername

--- app/Views/admin/users.php
+++ app/Views/admin/users.php
@@ -63,10 +63,10 @@
                   {
                     $user_manager = getUserMeta("user_manager", $row->user_id);
                     if(!empty($user_manager) && !empty($user_manager->meta_value)){
-                      $umanager1 = $user_manager->meta_value;
-                      $loggedinuserdata = getLoggedInUserData($umanager1);
-                      $umanager = $loggedinuserdata->user_name;
-                    }else{$umanager= "-";}
+                      $umanager = $user_manager->meta_value;
+                      //$loggedinuserdata = getLoggedInUserData($umanager1);
+                      //$umanager = $loggedinuserdata->user_name;
+                    }else{$umanager = "-";}
                     
                     echo "<tr>";
                     echo "<td>".$row->user_id."</td>";
